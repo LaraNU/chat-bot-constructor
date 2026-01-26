d72532d59f79c19248922c26b176a09cd2dc2dba
LaraNU <105387991+LaraNU@users.noreply.github.com>
2026-01-26 17:57:18 +0300

    Feat/3 setup database (#8)
    * feat: 3 - add prisma
    
    * refactor: 3 - update lint-staged setup

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from 'next';
 import { Inter, Roboto } from 'next/font/google';
 import './globals.css';
 import { hasLocale, Locale, NextIntlClientProvider } from 'next-intl';
