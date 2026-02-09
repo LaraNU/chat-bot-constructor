9ae9a128daa400405e90283f612b80edb329db97
Larisa Novozhilova <kara4516@gmail.com>
2026-02-09 14:45:25 +0300

    feat: 17 - add server client in middleware

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -3,14 +3,13 @@
 import { Search, Bell } from 'lucide-react';
 import { Input } from '@/shared/ui/input';
 import { Button } from '@/shared/ui/button';
-import { useTranslations, useLocale } from 'next-intl';
-import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+import { Link } from '@/i18n/navigation';
 import { ThemeToggle } from '@/features/theme-toggle';
 import { LangSwitcher } from '@/features/language-switcher';
 
 export function Header() {
   const t = useTranslations('Header');
-  const locale = useLocale();
 
   // Временная переменная
   const isAuth = false;
@@ -19,7 +18,7 @@ export function Header() {
     <header className="border-border bg-background/80 sticky top-0 z-50 w-full border-b backdrop-blur-sm">
       <div className="flex h-14 items-center justify-between px-4 md:px-6">
         <div className="flex items-center gap-6">
-          <Link href={`/${locale}`} className="flex items-center gap-2">
+          <Link href={`/`} className="flex items-center gap-2">
             <div className="bg-foreground flex h-8 w-8 items-center justify-center rounded-lg">
               <svg
                 viewBox="0 0 24 24"
@@ -66,10 +65,16 @@ export function Header() {
             <p>UserNav</p>
           ) : (
             <div className="flex items-center gap-2">
-              <Button variant="ghost" size="sm" className="hidden sm:flex">
-                {t('login')}
+              <Button variant="ghost" size="sm" className="sm:flex">
+                <Link href={'/sign-in'} type="button">
+                  {t('login')}
+                </Link>
               </Button>
-              <Button size="sm">{t('sign_up')}</Button>
+              <Button size="sm" asChild>
+                <Link href={'/sign-up'} type="button">
+                  {t('sign_up')}
+                </Link>
+              </Button>
             </div>
           )}
         </div>
